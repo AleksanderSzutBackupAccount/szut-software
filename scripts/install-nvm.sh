7e14115ea490e4730bf0f3f884352967d178d380
Aleksander Szut <szut.aleksander@gmail.com>
2023-09-01 06:21:36 +0200

    devops

--- scripts/install-nvm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash; \
-export NVM_DIR="$HOME/.nvm";
-source ~/.bash_profile;
-source $HOME/.nvm/nvm.sh;
-nvm --version
