dfdb6322339f7df0c9d9c911c5f2b817dcd89281
Aleksander Szut <szut.aleksander@gmail.com>
2023-09-01 06:21:06 +0200

    devops

--- scripts/checkversion.js
+++ scripts/checkversion.js
@@ -1,12 +1,13 @@
 const fs = require("fs");
-const { exec } = require("child_process");
 
 const nvmVersion = fs.readFileSync(".nvmrc").toString().trim();
-const npmVersion = fs.readFileSync(".npmrc").toString().trim();
 const desired = `v${nvmVersion}`;
 const running = process.version;
 
 if (!running.startsWith(desired)) {
-    exec("${NVM_DIR}/nvm.sh install")
-    exec("${NVM_DIR}/nvm.sh use")
+    console.error(
+        `You are running Node ${running} but version ${desired} is expected. ` +
+        `Use nvm or another version manager to install ${desired}, and then activate it.`
+    );
+    process.exit(1);
 }
